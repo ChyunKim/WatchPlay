fe833e83b9786108a7a11490bab9d2ce9c7aec91
ChyunKim <co1995@naver.com>
2022-09-18 22:50:44 +0900

    feat : 로그아웃 버튼 클릭시 로그아웃 되도록 구현

--- play-app/pages/accout.tsx
+++ play-app/pages/accout.tsx
@@ -22,20 +22,29 @@ const accout = () => {
   });
 
   const eventLogout = () => {
-
-  }
+    axios.get("api/logout").then((res) => {
+      if (res.status === 200) {
+        router.push("/");
+      }
+    });
+  };
 
   return (
     <>
       <Nav />
-      {isLogin && <div className="pt-28 bg-gray-800 text-white w-full text-lg pb-8">
-        <div className="block pl-20 w-full">
-          <span className="font-bold text-2xl">my account</span>
-          <button className="float-right mr-20 bg-red-500 hover:bg-red-600 text-white font-bold py-1.5 px-4 rounded" onClick={eventLogout}>
-            Logout
-          </button>
+      {isLogin && (
+        <div className="pt-28 bg-gray-800 text-white w-full text-lg pb-8">
+          <div className="block pl-20 w-full">
+            <span className="font-bold text-2xl">my account</span>
+            <button
+              className="float-right mr-20 bg-red-500 hover:bg-red-600 text-white font-bold py-1.5 px-4 rounded"
+              onClick={eventLogout}
+            >
+              Logout
+            </button>
+          </div>
         </div>
-      </div>}
+      )}
     </>
   );
 };
--- play-app/pages/api/logout.ts
+++ play-app/pages/api/logout.ts
@@ -1,9 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
     res.setHeader("Set-Cookie", "a_name=Me;Max-age=0;HttpOnly, Secure");
     res.statusCode = 200;
     res.json({ message: "ok" });
-  }
 };
