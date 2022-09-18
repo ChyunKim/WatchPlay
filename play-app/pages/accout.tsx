d4b082685faf9904d5a90e264d4771a4f84f3a46
ChyunKim <co1995@naver.com>
2022-09-18 20:56:03 +0900

    feat : 로그인이 안되어있을때 페이지 이동
    api/isLogin 으로 로그인이 되어있는지 확인 후 안되어있다면 register
    페이지로 이동

--- play-app/pages/accout.tsx
+++ play-app/pages/accout.tsx
@@ -1,3 +1,22 @@
-const accout = () => {};
+import axios from "axios";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+const accout = () => {
+  const router = useRouter();
+
+  const checkLogin = () => {
+    axios.get("api/isLogin").then((res) => {
+      if (res.status === 200 && res.data.name) {
+        // 로그인성공
+      } else {
+        router.push("/register");
+      }
+    });
+  };
+
+  useEffect(() => {
+    checkLogin();
+  });
+};
 
 export default accout;
--- play-app/pages/api/isLogin.ts
+++ play-app/pages/api/isLogin.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string|null
-}
+  name: string | null;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: null })
+  res.status(200).json({ name: null });
 }
