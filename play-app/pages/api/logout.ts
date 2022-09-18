5e327cf7ba02d80a97f3b9a9b4cdc7ebe0e3e43b
ChyunKim <co1995@naver.com>
2022-09-18 23:54:38 +0900

    feat : 로그아웃 api 수정

--- play-app/pages/api/logout.ts
+++ play-app/pages/api/logout.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-    res.setHeader("Set-Cookie", "a_name=Me;Max-age=0;HttpOnly, Secure");
-    res.statusCode = 200;
-    res.json({ message: "ok" });
+  res.setHeader("Set-Cookie", "a_name=Me;Max-age=0;HttpOnly, Secure");
+  res.statusCode = 200;
+  res.json({ message: "ok" });
 };
